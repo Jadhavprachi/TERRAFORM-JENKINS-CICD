1537aae1f0b2ddc369b944c954b297247001fcfb
AnilDevops23 <127133351+AnilDevops23@users.noreply.github.com>
2023-10-20 15:55:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "myawsbucket1007"
+    bucket         = "myawsbucket10078"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "anilDBTable"
