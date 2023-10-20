484a668b307743485736dacac83c3d7ef3b52af9
AnilDevops23 <127133351+AnilDevops23@users.noreply.github.com>
2023-10-20 15:55:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "myawsbucket1007"
+  default     = "myawsbucket10078"
 }
